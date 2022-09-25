1c5c72b1ebbf3aaade985232577a6d466438c24a
Sasho Serafimov <114098550+SashoS90@users.noreply.github.com>
2022-09-25 17:05:57 +0100

    Added comments
    Added comments to the functionality of the code.

--- Basic-Syntax-Conditional-Statements-and-Loops/01_find_the_largest.py
+++ Basic-Syntax-Conditional-Statements-and-Loops/01_find_the_largest.py
@@ -1,12 +1,24 @@
+# Input.
 number = int(input())
+
+# Empy list to add all the digits of the received integer.
 largest_number = []
+
+# Empty string for forming the final output of the output.
 largest_integer = ''
 
+# For loop through the string of the received input to find and add the highest digits into the largest number list.
 for digit in str(number):
     for highest_digit in range(9, -1, -1):
         if int(digit) == highest_digit:
             largest_number.append(digit)
             break
+
+# Sorting the largest number list from highest to lowest numbers.
 largest_number.sort(reverse=True)
+
+# Joining the separate elements in the list to form the final output.
 largest_integer = ''.join(largest_number)
+
+# Final output.
 print(int(largest_integer))
